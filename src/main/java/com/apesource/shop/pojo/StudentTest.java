325be63ecdaf3831023edb1baf7ebc84f91226c3
huangxu123456 <458579044@qq.com>
2020-03-31 22:24:32 +0800

    测试提交

---
+++ src/main/java/com/apesource/shop/pojo/StudentTest.java
@@ -0,0 +1,37 @@
+package com.apesource.shop.pojo;
+
+public class StudentTest {
+	private int id;
+	private String name;
+	private int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public StudentTest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public StudentTest(int id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	
+
+}
