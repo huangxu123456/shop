14eba727e208e4629b2544da4c12ecc2e208a173
huangxu123456 <458579044@qq.com>
2020-04-01 22:05:21 +0800

    add:adress属性

--- src/main/java/com/apesource/shop/pojo/StudentTest.java
+++ src/main/java/com/apesource/shop/pojo/StudentTest.java
@@ -4,11 +4,19 @@ public class StudentTest {
 	private int id;
 	private String name;
 	private int age;
+	private String address;
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
 	}
 	public String getName() {
 		return name;
